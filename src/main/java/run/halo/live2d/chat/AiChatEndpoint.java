d5d3bc16142f9188883dc4f13961ff641ecd1d38
1254qwer <33991546+1254qwer@users.noreply.github.com>
2025-10-22 17:18:41 +0800

    system prompt不为空，更改兜底方案

--- src/main/java/run/halo/live2d/chat/AiChatEndpoint.java
+++ src/main/java/run/halo/live2d/chat/AiChatEndpoint.java
@@ -123,7 +123,8 @@ public class AiChatEndpoint implements CustomEndpoint {
     record AiChatBaseSetting(boolean isAnonymous, String systemMessage) {
         AiChatBaseSetting {
             if (StringUtils.isBlank(systemMessage)) {
-                throw new IllegalArgumentException("system message must not be null");
+                systemMessage = "you are a help assistant.";
+                log.warn("system message is null or blank, using default value");
             }
         }
     }
